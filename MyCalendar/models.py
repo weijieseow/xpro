3ab378b6a8f06e9c9112df00ac9dd8a9cba97df5
“tyean” <tyean@users.noreply.github.com>
2016-06-20 16:26:35 +0800

    reset database
    deleted db.sqlite3 and migrations folder, migrated once

--- MyCalendar/models.py
+++ MyCalendar/models.py
@@ -6,9 +6,9 @@ from datetime import date, timedelta
 class Event(models.Model):
     event_name = models.CharField(max_length = 255)
     start_date = models.DateField(null=True, default=date.today())
-    start_time = models.TimeField(null=True, default=date.today())
+    start_time = models.TimeField(null=True)
     end_date = models.DateField(null=True, default=date.today())
-    end_time = models.TimeField(null=True, default=date.today() + timedelta(minutes=30))
+    end_time = models.TimeField(null=True)
     description = models.TextField(blank=True)
 
 

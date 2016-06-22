abd28bd66f311bd60f9cae141fc33d9b36388725
“tyean” <tyean@users.noreply.github.com>
2016-06-22 20:16:33 +0800

    enabled event creation
    added widget for time selection, and added functionality for user
    creation of event through a form. Adjusted Event model, particularly to
    properly link Event to User

--- MyCalendar/models.py
+++ MyCalendar/models.py
@@ -4,26 +4,27 @@ from django.db import models
 from datetime import date, timedelta
 from django.contrib.auth.models import User
 
-'''
+
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, editable=False)
 
     # The additional attributes we wish to include.
-    bio= models.TextField(blank=True)
+    bio = models.TextField(blank=True)
 
     # Override the __unicode__() method to return out something meaningful!
     def __str__(self):
         return self.user.username
-'''
+
 
 class Event(models.Model):
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     event_name = models.CharField(max_length = 255)
     start_date = models.DateField(null=True, default=date.today())
     start_time = models.TimeField(null=True)
-    end_date = models.DateField(null=True, default=date.today() + timedelta(minutes=30))
-    end_time = models.TimeField(null=True)
+    end_date = models.DateField(null=True, default=date.today(),
+                                help_text='The end date must be later than the start date')
+    end_time = models.TimeField(null=True, help_text='The end must be later than the start ')
     description = models.TextField(blank=True)
 
     def __str__(self):

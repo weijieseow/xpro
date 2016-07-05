b6824e20e7a0b6de11076b5366732a262f8d1a65
weijieseow <sweijie94@gmail.com>
2016-07-05 12:40:35 +0800

    test

--- xpro/urls.py
+++ xpro/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'MyCalendar.views.loginView'),
     url(r'^admin/', admin.site.urls),
     url(r'^MyCalendar/', include('MyCalendar.urls')),
-
 ]

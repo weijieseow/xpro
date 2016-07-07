f9b04c677623d88b8ae0f4385692798b9edf2437
weijieseow <sweijie94@gmail.com>
2016-07-07 16:03:24 +0800

    test

--- xpro/settings.py
+++ xpro/settings.py
@@ -122,7 +122,7 @@ DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.

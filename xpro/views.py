8dbf1032c41aa50ccf7bc38b8a290151a7ee1cce
weijieseow <sweijie94@gmail.com>
2016-07-15 00:51:07 +0800

    changed buttons and index routing

--- xpro/views.py
+++ xpro/views.py
@@ -6,13 +6,13 @@ from django.shortcuts import render, redirect
 
 def custom_login(request, **kwargs):
     if request.user.is_authenticated():
-        return redirect('MyCalendar:tasklist')
+        return redirect('MyCalendar:home')
     else:
         return login(request, **kwargs)
 
 
 def index_view(request):
     if request.user.is_authenticated():
-        return redirect('MyCalendar:tasklist')
+        return redirect('MyCalendar:home')
     else:
         return render(request, 'index.html')

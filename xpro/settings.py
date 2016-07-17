7734c4490d1280adec2b6998c8a1f352ab89d5d7
“tyean” <tyean@users.noreply.github.com>
2016-07-18 02:37:02 +0800

    edited settings.py for heroku testing

--- xpro/settings.py
+++ xpro/settings.py
@@ -151,12 +151,12 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.

905434a3af6571d67db4778f193e030f56d238e4
weijieseow <sweijie94@gmail.com>
2016-07-24 17:21:13 +0800

    fine tuned

--- MyCalendar/views.py
+++ MyCalendar/views.py
@@ -387,6 +387,14 @@ class projectTaskUpdateView(edit.UpdateView):
         else:
             return self.post(self, request)
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(projectTaskUpdateView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['project_id'] = self.get_object().project.pk
+
+        return context
+
 
 
 
@@ -397,6 +405,14 @@ class projectTaskDeleteView(edit.DeleteView):
 
     def get_success_url(self):
         return reverse('MyCalendar:project_tasklist', kwargs={'project_id': self.object.project.pk})
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(projectTaskDeleteView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['project_id'] = self.get_object().project.pk
+
+        return context
 
 
 @login_required
